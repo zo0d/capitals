8977606e9d49b378ed7b09eb50271b9ee6c0aef6
BJ <bj@zo0d-2.local>
2019-05-21 08:57:18 -0400

    Init

---
+++ Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  capitals
+//
+//  Created by BJ on 2019-05-21.
+//  Copyright © 2019 BJ. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
